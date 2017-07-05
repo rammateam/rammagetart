68b5a60ab048439b03fdfe6829f1836c37881b9c
Mohammed Naji <moha.buy@gmail.com>
2017-07-05 23:21:13 +0300

    fix problem

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   APP_ID: '781400568651772',
-  URL: ' https://thegamesdb.net/api/GetArt.php?id=2'
+  URL: ' http://thegamesdb.net/api/GetArt.php?id=2'
 }
--- src/fronEnd/actions.js
+++ src/fronEnd/actions.js
@@ -1,21 +1,31 @@
 import {URL} from './../../config.js'
-const xml2js = require('xml2js');
+// const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
-const getApiData = () => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const xml = xhttp.responseText;
-      parser.parseString(xml, (err, result) => {
-        const url = result.Data.baseImgUrl[0];
-        const images = result.Data.Images[0].fanart.map((data) => url + data.original[0]._);
-        console.log(images);
-      });
-    }
-  };
-  xhttp.open("GET", URL, true);
-  xhttp.send();
-}
-export {
-  getApiData
-}
+const request = require('request');
+request('http://thegamesdb.net/api/GetArt.php?id=2', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+
+const getApiData=body;
+  export {
+    getApiData
+  }
+
+});
+
+// const getApiData = () => {
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       const xml = xhttp.responseText;
+//       parser.parseString(xml, (err, result) => {
+//         const url = result.Data.baseImgUrl[0];
+//         const images = result.Data.Images[0].fanart.map((data) => url + data.original[0]._);
+//         console.log(images);
+//       });
+//     }
+//   };
+//   xhttp.open("GET", URL, true);
+//   xhttp.send();
+// }
