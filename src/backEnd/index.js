9bee8e2ed6cde93639e90a1fcb6496fbf4c953d3
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-07-06 22:20:25 +0300

    handle error and change root to /image

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -9,17 +9,18 @@ var images = [];
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/images', (req, res) => {
   const fullImages = () => {
     const id = Math.floor(Math.random() * 1000) + 1;
     getImages(id, (err, result) => {
+      if(err) res.end({'err': 'error fetching images'}).status(400);
       const url = result.Data.baseImgUrl[0];
       if (result.Data.Images[0].fanart) {
         images = images.concat(result.Data.Images[0].fanart.map((data) => url + data.original[0]._));
-        if (images.length < 5) {
+        if (images.length < 3) {
           fullImages();
         } else {
-          res.send(images)
+          res.end(images)
         }
       }
     });
