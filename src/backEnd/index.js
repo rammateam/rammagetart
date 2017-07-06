92b0c2d8bd31b43ab7f9c150f17063d601431a80
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-07-06 23:46:27 +0300

    fix error

--- src/backEnd/index.js
+++ src/backEnd/index.js
@@ -10,17 +10,21 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/images', (req, res) => {
+  console.log('ssss');
   const fullImages = () => {
-    const id = Math.floor(Math.random() * 1000) + 1;
+    const id = (Math.floor(Math.random() * 1000) + 1).toString();
     getImages(id, (err, result) => {
-      if(err) res.end({'err': 'error fetching images'}).status(400);
+      if(err) {
+        console.log('err',err);
+        return res.end({'err': 'error fetching images'}).status(400);
+      }
       const url = result.Data.baseImgUrl[0];
       if (result.Data.Images[0].fanart) {
         images = images.concat(result.Data.Images[0].fanart.map((data) => url + data.original[0]._));
         if (images.length < 3) {
           fullImages();
         } else {
-          res.end(images)
+          return res.json(images);
         }
       }
     });
