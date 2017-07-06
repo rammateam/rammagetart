ed65d7ea811907fefc4f67c8d80a59b9bf4226a0
Mohammed Naji <moha.buy@gmail.com>
2017-07-06 11:28:05 +0300

    fix

--- src/fronEnd/actions.js
+++ src/fronEnd/actions.js
@@ -2,22 +2,22 @@ import {URL} from './../../config.js'
 const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
 const getApiData = () => {
-  // const xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //
-  //   if (this.readyState == 4 && this.status == 200) {
-  //
-  //     const xml = xhttp.responseText;
-  //     parser.parseString(xml, (err, result) => {
-  //       const url = result.Data.baseImgUrl[0];
-  //       const images = result.Data.Images[0].fanart.map((data) => url + data.original[0]._);
-  //       console.log(images);
-  //     });
-  //   }
-  // };
-  // xhttp.open("GET", URL, true);
-  // xhttp.setRequestHeader("Access-Control-Allow-Origin", 'http://thegamesdb.net')
-  // xhttp.send();
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+      const xml = xhttp.responseText;
+      parser.parseString(xml, (err, result) => {
+        const url = result.Data.baseImgUrl[0];
+        const images = result.Data.Images[0].fanart.map((data) => url + data.original[0]._);
+        console.log(images);
+      });
+    }
+  };
+  xhttp.open("GET", URL, true);
+  xhttp.setRequestHeader("Access-Control-Allow-Origin", 'http://thegamesdb.net')
+  xhttp.send();
 }
 export {
   getApiData
