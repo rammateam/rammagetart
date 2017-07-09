cc5f06c80fdb9c3038722cd7eb9fb097239fe7f1
4daliaomry4 <alaakhattab92@gmail.com>
2017-07-09 17:24:14 +0300

    add share button to the footer

--- src/fronEnd/components/app.jsx
+++ src/fronEnd/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SimpleSlider from './slidShow.jsx';
 import ShareButton from './shareFacebook.jsx';
-import {Button, Icon,Popup} from 'semantic-ui-react';
+import {Button, Icon, Popup} from 'semantic-ui-react';
 import getImages from '../actions.js';
 import randomImage from '../utils/utils.js';
 const defaultImages = randomImage();
@@ -11,7 +11,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       images: defaultImages,
-      displayDownload : false,
+      displayDownload: false,
       shareBG: 'grey'
     };
     this.onShare = this.onShare.bind(this)
@@ -27,38 +27,34 @@ class App extends React.Component {
       displayShare: !this.state.displayShare
     })
   }
-onDownload(){
-  this.setState({
-    displayDownload: !this.state.displayDownload
-  })
-}
+  onDownload() {
+    this.setState({
+      displayDownload: !this.state.displayDownload
+    })
+  }
   render() {
     return (
       <div>
         <SimpleSlider imagesList={this.state.images}/>
-          <div className='icon'>
-              <a href="https://www.facebook.com/" target="_blank">
-                <Popup className='popup'trigger={< Button onClick = {
-                  this.onFacebook
-                }
-                circular color = 'facebook' icon='facebook'>< /Button>}
-                content='Visit us'
-                position='right center'
-                />
-              </a>
-            </div>
-          <div className='share-icon' onMouseOver= {() => {this.setState({shareBG:'blue'})}} onMouseLeave={() => {this.setState({shareBG:'grey'})}}>
-            <div>
-              <Button circular onClick={this.onShare} color= {this.state.shareBG} size='mini' icon='share alternate'></Button>
+        <div className='share-icon' onMouseOver= {() => {this.setState({shareBG:'blue'})}} onMouseLeave={() => {
+          this.setState({shareBG: 'grey'})
+        }}>
+          <div>
+            <Button style={{
+              backgroundColor: '#323232',
+              width: '22px',
+              height: '22px'
+            }} circular onClick={this.onShare} color={this.state.shareBG} size='mini' icon='share alternate'>
+            </Button>
 
-              </div>
-              {this.state.displayShare
-                ? <div className='share-button'>
-                    <ShareButton/>
-                  </div>
-                : null
-               }
           </div>
+          {this.state.displayShare
+            ? <div className='share-button'>
+                <ShareButton/>
+              </div>
+            : null
+}
+        </div>
       </div>
     )
   }
